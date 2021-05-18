2d537eec69eb037f4f07a5566cd8563f2cbba549
Ben Lurye <69798922+benlurye@users.noreply.github.com>
2021-05-18 17:18:26 -0400

    lots!

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#JiSlider').JiSlider(
+        // autoplay
+        "auto:", true,
+        // initial image
+        "start:", 1,
+        // animation speed
+        "time:", 600,
+        // transition delay
+        "stay:", 3000,
+        // display slider controls
+        "controller:", true,
+        // easing method
+        "easing:", 'ease',
+        // text color
+        "color:", '#fff',
+        // reverse direction
+        "reverse:", false,
+        )
+
+})
