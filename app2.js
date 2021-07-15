e9aea630464c2d95c03433b7e8b063496e32cace
Ben Lurye <benlurye@gmail.com>
2021-07-15 11:52:39 -0400

    update app / favicon

--- app2.js
+++ app2.js
@@ -7,7 +7,6 @@ var darken = new IntersectionObserver(function(entries) {
 	// since there is a single target to be observed, there will be only one entry
 	if(entries[0]['isIntersecting'] === true) {
 		if(entries[0]['intersectionRatio'] === 1)
-			console.log('Target is fully visible in the screen');
             logo.classList.add("dark");
 	}
 }, { threshold: [0, 0.5, 1] });
